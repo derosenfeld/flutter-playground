e65ae7f37f53febb498fcdae556ed38788dfb941
Daniel Rosenfeld <derosenfeld@gmail.com>
2022-12-29 19:19:04 +0000

    add chart and table demos

--- playground/lib/main.dart
+++ playground/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pluto_grid/pluto_grid.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +13,209 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'data playground',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Playground Home Page'),
+      home: const DataApp(title: 'stats.derlcl.com'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class DataApp extends StatefulWidget {
+  const DataApp({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DataApp> createState() => _DataAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _DataAppState extends State<DataApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Row(children: const <Widget>[
+        Expanded(child: PlutoGridExampleBox()),
+        Expanded(child: LineChartWidget()),
+      ]),
+    );
   }
+}
+
+/// PlutoGrid Widget
+//
+/// For more examples, go to the demo web link on the github below.
+class PlutoGridExampleBox extends StatefulWidget {
+  const PlutoGridExampleBox({Key? key}) : super(key: key);
+
+  @override
+  State<PlutoGridExampleBox> createState() => _PlutoGridExampleBoxState();
+}
+
+class _PlutoGridExampleBoxState extends State<PlutoGridExampleBox> {
+  final List<PlutoColumn> columns = <PlutoColumn>[
+    PlutoColumn(
+      title: 'Id',
+      field: 'id',
+      type: PlutoColumnType.text(),
+    ),
+    PlutoColumn(
+      title: 'Name',
+      field: 'name',
+      type: PlutoColumnType.text(),
+    ),
+    PlutoColumn(
+      title: 'Age',
+      field: 'age',
+      type: PlutoColumnType.number(),
+    ),
+    PlutoColumn(
+      title: 'Role',
+      field: 'role',
+      type: PlutoColumnType.select(<String>[
+        'Programmer',
+        'Designer',
+        'Owner',
+      ]),
+    ),
+    PlutoColumn(
+      title: 'Joined',
+      field: 'joined',
+      type: PlutoColumnType.date(),
+    ),
+    PlutoColumn(
+      title: 'Working time',
+      field: 'working_time',
+      type: PlutoColumnType.time(),
+    ),
+    PlutoColumn(
+      title: 'salary',
+      field: 'salary',
+      type: PlutoColumnType.currency(),
+      footerRenderer: (rendererContext) {
+        return PlutoAggregateColumnFooter(
+          rendererContext: rendererContext,
+          formatAsCurrency: true,
+          type: PlutoAggregateColumnType.sum,
+          format: '#,###',
+          alignment: Alignment.center,
+          titleSpanBuilder: (text) {
+            return [
+              const TextSpan(
+                text: 'Sum',
+                style: TextStyle(color: Colors.red),
+              ),
+              const TextSpan(text: ' : '),
+              TextSpan(text: text),
+            ];
+          },
+        );
+      },
+    ),
+  ];
+
+  final List<PlutoRow> rows = [
+    PlutoRow(
+      cells: {
+        'id': PlutoCell(value: 'user1'),
+        'name': PlutoCell(value: 'Mike'),
+        'age': PlutoCell(value: 20),
+        'role': PlutoCell(value: 'Programmer'),
+        'joined': PlutoCell(value: '2021-01-01'),
+        'working_time': PlutoCell(value: '09:00'),
+        'salary': PlutoCell(value: 300),
+      },
+    ),
+    PlutoRow(
+      cells: {
+        'id': PlutoCell(value: 'user2'),
+        'name': PlutoCell(value: 'Jack'),
+        'age': PlutoCell(value: 25),
+        'role': PlutoCell(value: 'Designer'),
+        'joined': PlutoCell(value: '2021-02-01'),
+        'working_time': PlutoCell(value: '10:00'),
+        'salary': PlutoCell(value: 400),
+      },
+    ),
+    PlutoRow(
+      cells: {
+        'id': PlutoCell(value: 'user3'),
+        'name': PlutoCell(value: 'Suzi'),
+        'age': PlutoCell(value: 40),
+        'role': PlutoCell(value: 'Owner'),
+        'joined': PlutoCell(value: '2021-03-01'),
+        'working_time': PlutoCell(value: '11:00'),
+        'salary': PlutoCell(value: 700),
+      },
+    ),
+  ];
+
+  /// columnGroups that can group columns can be omitted.
+  final List<PlutoColumnGroup> columnGroups = [
+    PlutoColumnGroup(title: 'Id', fields: ['id'], expandedColumn: true),
+    PlutoColumnGroup(title: 'User information', fields: ['name', 'age']),
+    PlutoColumnGroup(title: 'Status', children: [
+      PlutoColumnGroup(title: 'A', fields: ['role'], expandedColumn: true),
+      PlutoColumnGroup(title: 'Etc.', fields: ['joined', 'working_time']),
+    ]),
+  ];
+
+  /// [PlutoGridStateManager] has many methods and properties to dynamically manipulate the grid.
+  /// You can manipulate the grid dynamically at runtime by passing this through the [onLoaded] callback.
+  late final PlutoGridStateManager stateManager;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: PlutoGrid(
+          columns: columns,
+          rows: rows,
+          columnGroups: columnGroups,
+          onLoaded: (PlutoGridOnLoadedEvent event) {
+            stateManager = event.stateManager;
+            stateManager.setShowColumnFilter(true);
+          },
+          onChanged: (PlutoGridOnChangedEvent event) {
+            print(event);
+          },
+          configuration: const PlutoGridConfiguration(),
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+    );
+  }
+}
+
+// Chart Widget
+class LineChartWidget extends StatelessWidget {
+  const LineChartWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2,
+      child: LineChart(
+        LineChartData(
+          lineBarsData: [
+            LineChartBarData(
+              spots: [
+                FlSpot(0, 0),
+                FlSpot(1, 1),
+                FlSpot(2, 4),
+              ],
+              isCurved: false,
+              // dotData: FlDotData(
+              //   show: false,
+              // ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
